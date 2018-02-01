74cf9dd53fb5015095759bde5fb224964c535fb9
Muchammad Agung Laksana <m.agung.laksana@gmail.com>
2018-02-01 11:07:27 +0700

    Initial Commit

---
+++ HRIS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HRIS
+//
+//  Created by Muchammad Agung Laksana on 2/1/18.
+//  Copyright © 2018 Muchammad Agung Laksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
